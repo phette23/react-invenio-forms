e88956f4b7b147e3b23179f28bb328e2daf387d1
Guillaume Viger <fenekku@fenekku.com>
2020-05-21 14:04:42 -0500

    field: add RadioField (moved changes from RDM)

---
+++ src/lib/core/RadioField.test.js
@@ -0,0 +1,33 @@
+// This file is part of React-Invenio-Forms
+// Copyright (C) 2020 CERN.
+// Copyright (C) 2020 Northwestern University.
+//
+// React-Invenio-Forms is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { RadioField } from './RadioField';
+
+import { Form, Formik } from 'formik';
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <Formik>
+        {(props) => (
+          <Form>
+            <RadioField checked={true}
+                fieldPath="testFieldPath"
+                label="testLabel"
+                labelIcon="money"
+                optimized={false}
+                onChange={(e, {value}) => null}
+                value="testValue"
+            />
+          </Form>
+        )}
+      </Formik>,
+      div
+    );
+});
