d76b10d1992fd0c01ec4d3ab4650d4c9692a5ded
Unix <mb_wali@hotmail.com>
2021-02-01 20:10:49 +0100

    TextField: helptext for input

--- src/lib/TextField.js
+++ src/lib/TextField.js
@@ -1,6 +1,7 @@
 // This file is part of React-Invenio-Forms
 // Copyright (C) 2020-2021 CERN.
 // Copyright (C) 2020-2021 Northwestern University.
+// Copyright (C) 2021 Graz University of Technology
 //
 // React-Invenio-Forms is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
@@ -12,8 +13,9 @@ import { Form } from 'semantic-ui-react';
 
 export class TextField extends Component {
   renderFormField = (formikBag) => {
-    const { fieldPath, optimized, error, ...uiProps } = this.props;
+    const { fieldPath, optimized, error, helpText, ...uiProps } = this.props;
     return (
+      <>
       <Form.Input
         id={fieldPath}
         name={fieldPath}
@@ -23,6 +25,10 @@ export class TextField extends Component {
         error={error || getIn(formikBag.form.errors, fieldPath, null)}
         {...uiProps}
       />
+        <label className="helptext">
+          {helpText}
+        </label>
+      </>
     );
   };
 
@@ -43,8 +49,10 @@ TextField.propTypes = {
   fieldPath: PropTypes.string.isRequired,
   optimized: PropTypes.bool,
   width: PropTypes.number,
+  helpText: PropTypes.string
 };
 
 TextField.defaultProps = {
   optimized: false,
+  helpText: ''
 };
