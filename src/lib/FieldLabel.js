65ca7272d69267072b1b217f6ddd2b1a76dc36ef
Unix <mb_wali@hotmail.com>
2021-02-05 11:36:53 +0100

    FieldLabels: injecting classname for labels
    (note) most top-level fields are created with this component.

--- src/lib/FieldLabel.js
+++ src/lib/FieldLabel.js
@@ -1,6 +1,7 @@
 // This file is part of React-Invenio-Forms
 // Copyright (C) 2020 CERN.
 // Copyright (C) 2020 Northwestern University.
+// Copyright (C) 2021 Graz University of Technology.
 //
 // React-Invenio-Forms is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
@@ -11,9 +12,9 @@ import { Icon } from 'semantic-ui-react';
 
 export class FieldLabel extends Component {
   render() {
-    const { htmlFor, icon, label } = this.props;
+    const { htmlFor, icon, label, className } = this.props;
     return (
-      <label htmlFor={htmlFor}>
+      <label htmlFor={htmlFor} className={className}>
         {icon ? <Icon name={icon} /> : null}
         {label}
       </label>
@@ -25,4 +26,9 @@ FieldLabel.propTypes = {
   htmlFor: PropTypes.string,
   label: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
   labelIcon: PropTypes.string,
+  className: PropTypes.string,
 };
+
+FieldLabel.defaultProps = {
+  className: 'field-label-class'
+}
