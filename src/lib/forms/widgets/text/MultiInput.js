c9f019c40ed54fcdd7786b09dd631cc51de08175
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2022-09-14 17:41:05 +0200

    widgets: add MultiInput component

---
+++ src/lib/forms/widgets/text/MultiInput.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { useFormikContext, getIn } from "formik";
+
+import { FieldLabel } from "../../FieldLabel";
+import { SelectField } from "../../SelectField";
+
+export default function MultiInput({
+  additionLabel,
+  description,
+  placeholder,
+  fieldPath,
+  label,
+  icon,
+  required,
+}) {
+  const [options, setOptions] = useState([]);
+  const { values } = useFormikContext();
+  const serializeValues = (values) =>
+    values?.map((item) => ({
+      text: item,
+      key: item,
+      value: item,
+    }));
+
+  return (
+    <>
+      <SelectField
+        fieldPath={fieldPath}
+        label={<FieldLabel htmlFor={fieldPath} icon={icon} label={label} />}
+        options={serializeValues(getIn(values, fieldPath, []))}
+        placeholder={placeholder}
+        required={required}
+        search
+        multiple
+        clearable
+        optimized
+        defaultValue={[]}
+        noResultsMessage={null} // hide the no results message
+        additionLabel={additionLabel}
+        onChange={({ data, formikProps }) => {
+          setOptions(serializeValues(data.value));
+          formikProps.form.setFieldValue(fieldPath, data.value);
+        }}
+        allowAdditions
+        onAddItem={({ data }) => {
+          setOptions([{ text: data.value, value: data.value }, ...options]);
+        }}
+      />
+      {description && <label className="helptext">{description}</label>}
+    </>
+  );
+}
+
+MultiInput.propTypes = {
+  fieldPath: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  additionLabel: PropTypes.string,
+  icon: PropTypes.string,
+  required: PropTypes.bool,
+};
+
+MultiInput.defaultProps = {
+  additionLabel: undefined,
+  icon: undefined,
+  required: false,
+};
--- src/lib/forms/widgets/text/index.js
+++ src/lib/forms/widgets/text/index.js
@@ -1,3 +1,4 @@
 export { default as RichInput } from "./RichInput";
 export { default as TextArea } from "./TextArea";
 export { default as Input } from "./Input";
+export { default as MultiInput } from "./MultiInput";
