0b94251d9f365b3305fe0a2cfb32f884ce38b0be
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2022-11-10 17:44:16 +0100

    api: fix import

--- src/lib/api/index.js
+++ src/lib/api/index.js
@@ -4,4 +4,4 @@
 // React-Invenio-Forms is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
 
-export { http } from "./config";
+export { http } from "./connector";
