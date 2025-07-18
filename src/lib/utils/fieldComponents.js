898249860b42d4ab7cd644c0a62031efa036cf36
Pal Kerecsenyi <pal@p.kerecs.com>
2025-07-18 08:48:40 +0200

    doc: add jsdoc comment for `dynamicParametrize`

--- src/lib/utils/fieldComponents.js
+++ src/lib/utils/fieldComponents.js
@@ -1,6 +1,21 @@
 import { useFormikContext } from "formik";
 import * as React from "react";
 
+/**
+ * Override the parameters of a component (similarly to `parametrize`) using certain available
+ * context variables.
+ *
+ * Currently, the following arguments are passed into the `propsFactory` function:
+ *   - `formValues`: an object of the values in the Formik form state
+ *
+ * The `propsFactory` function should return an object of the props to be used for overriding the
+ * specified `Component`
+ *
+ * This function is adapted from `react-overridable`: https://github.com/indico/react-overridable/blob/d1ecfbec9993a461717824259900eec82d59f1e0/src/overridable.js#L7-L33
+ *
+ * @param Component - the React component to override the parameters, as in `parametrize` from react-overridable.
+ * @param {function} propsFactory - function returning the props to override the component with
+ */
 export function dynamicParametrize(Component, propsFactory) {
   const ParametrizedComponent = (props) => {
     const { values } = useFormikContext();
